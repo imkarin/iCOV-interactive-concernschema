d2073532fc1655fd1ed2386d805979fa982c8ad4
karinmeijvogel <karinmeijvogel@hotmail.com>
2020-05-15 18:24:06 +0200

    :tada: Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <nav>
+          <img src="https://icov.nl/wp-content/themes/icov/img/iCOV.svg" alt="iCOV logo"></img>
+          <a href="/">Link</a>
+          <a href="/">Link</a>
+          <a href="/">Link</a>
+          <a href="/">Link</a>
+        </nav>
+      </header>
+
+      <main>
+        <section>Filter section</section>
+        <section>Data view section</section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
