8b9ccb9ac0833101f1edd97a5614c982c219b688
karinmeijvogel <karinmeijvogel@hotmail.com>
2020-05-25 13:28:31 +0200

    :construction: D3 network chain now works within a React component

--- src/components/Datasection/Datasection.js
+++ src/components/Datasection/Datasection.js
@@ -1,12 +1,305 @@
-import React from 'react';
+import React, { Component } from 'react';
+import * as d3 from 'd3';
 import './Datasection.css';
+import data from '../data/flare.xml';
 
-function Datasection() {
-  return (
-    <div className="Datasection">
-      <p>Datasection</p>
-    </div>
-  );
+class Datasection extends Component {
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+
+  componentDidMount() {
+    let dataSection = d3.select(this.myRef.current);
+    const width = 1500,
+    height = 1250;
+
+    const svg = dataSection.append("svg")
+      .attr("width", width)
+      .attr("height", height)
+      .style("background-color", "#f7f7f7");
+
+    let simulation = d3.forceSimulation()
+      .force("link", d3.forceLink().id((d) => { return d.entityId }))
+      .force("charge", d3.forceManyBody().strength(-75))
+      .force("center", d3.forceCenter(width / 2, height / 2))
+      .on("tick", ticked)
+      .stop();
+
+    let radius = d3.scaleSqrt()
+      .domain([0, 20000])
+      .range([0, 20]);
+
+    let link = svg.append("g")
+      .attr("class", "link")
+      .attr("stroke", "#999")
+      .attr("stroke-width", .4)
+      .attr("stroke-opacity", 1)
+      .selectAll("line");
+
+    let node = svg.append("g")
+      .attr("class", "node")
+      .selectAll("circle");
+
+    let drag = d3.drag()
+      .on("start", dragstarted)
+      .on("drag", dragged)
+      .on("end", dragended);
+
+    d3.xml(data)
+    .then((document) => {
+      let entities = [];
+        class Entity {
+          constructor(
+            label,
+            description,
+            entityId,
+            identity,
+            labelIsIdentity,
+            city,
+            country,
+            dateOfBirth,
+            dateOfDeath,
+            departmentId,
+            departmentName,
+            edgeLabel,
+            edgeNotes1,
+            edgeNotes2,
+            firstName,
+            fromDate,
+            id,
+            lastName,
+            locationId,
+            managerId,
+            notes1,
+            notes2,
+            notities1,
+            notities2,
+            numberOfStaff,
+            postalCode,
+            sex,
+            streetAddress,
+            toDate,
+            type,
+            icovNodeId,
+            icovNodeType,
+            icovNodeSubtype,
+            icovNodeOrFlag,
+            icovNodeKey,
+            icovAditumQuery,
+            group
+          ) {
+            this.label = label;
+            this.description = description;
+            this.entityId = entityId;
+            this.identity = identity;
+            this.labelIsIdentity = labelIsIdentity;
+            this.city = city;
+            this.country = country;
+            this.dateOfBirth = dateOfBirth;
+            this.dateOfDeath = dateOfDeath;
+            this.departmentId = departmentId;
+            this.departmentName = departmentName;
+            this.edgeLabel = edgeLabel;
+            this.edgeNotes1 = edgeNotes1;
+            this.edgeNotes2 = edgeNotes2;
+            this.firstName = firstName;
+            this.fromDate = fromDate;
+            this.id = id;
+            this.lastName = lastName;
+            this.locationId = locationId;
+            this.managerId = managerId;
+            this.notes1 = notes1;
+            this.notes2 = notes2;
+            this.notities1 = notities1;
+            this.notities2 = notities2;
+            this.numberOfStaff = numberOfStaff;
+            this.postalCode = postalCode;
+            this.sex = sex;
+            this.streetAddress = streetAddress;
+            this.toDate = toDate;
+            this.type = type;
+            this.icovNodeId = icovNodeId;
+            this.icovNodeType = icovNodeType;
+            this.icovNodeSubtype = icovNodeSubtype;
+            this.icovNodeOrFlag = icovNodeOrFlag;
+            this.icovNodeKey = icovNodeKey;
+            this.icovAditumQuery = icovAditumQuery;
+            this.group = group;
+          }
+        }
+
+        d3.select(document).selectAll("Entity").each(function () {
+          const rawAttributes = this.closest("ChartItem").getElementsByTagName("Attribute")
+          let attributes = {}
+
+          Array.from(rawAttributes).forEach(attr => {
+            const AttributeName = attr.getAttribute("AttributeClassReference")
+            const AttributeValue = attr.getAttribute("Value")
+            attributes[AttributeName] = AttributeValue
+          })
+
+          entities.push(new Entity(
+            this.closest("ChartItem").getAttribute("Label"),
+            this.closest("ChartItem").getAttribute("Description"),
+            this.getAttribute("EntityId"),
+            this.getAttribute("Identity"),
+            this.getAttribute("LabelIsIdentity"),
+            attributes._CITY,
+            attributes._COUNTRY,
+            attributes._DATE_OF_BIRTH,
+            attributes._DATE_OF_DEATH,
+            attributes._DEPARTMENT_ID,
+            attributes._DEPARTMENT_NAME,
+            attributes._EDGE_LABEL,
+            attributes._EDGE_NOTES1,
+            attributes._EDGE_NOTES2,
+            attributes._FIRST_NAME,
+            attributes._FROM_DATE,
+            attributes._ID,
+            attributes._LAST_NAME,
+            attributes._LOCATION_ID,
+            attributes._MANAGER_ID,
+            attributes._NOTES1,
+            attributes._NOTES2,
+            attributes._NOTITIES1,
+            attributes._NOTITIES2,
+            attributes._NUMBER_OF_STAFF,
+            attributes._POSTAL_CODE,
+            attributes._SEX,
+            attributes._STREET_ADDRESS,
+            attributes._TO_DATE,
+            attributes._TYPE,
+            attributes.iCOV_node_id,
+            attributes.iCOV_node_type,
+            attributes.iCOV_node_subtype,
+            attributes.iCOV_node_or_flag,
+            attributes.iCOV_node_key,
+            attributes.iCOV_Aditum_Query,
+            Math.ceil(Math.random() * 3)
+          ));
+        })
+
+        let connections = []
+        class Connection {
+          constructor(source, target) {
+            this.source = source;
+            this.target = target;
+          }
+        }
+
+        d3.select(document).selectAll("Link").each(function() {
+          connections.push(new Connection(
+            this.getAttribute("End1Id"),
+            this.getAttribute("End2Id")
+          ))
+        })
+
+        let nodes = entities,
+            links = connections
+
+        link = link.data(links).enter().append("line")
+          .attr("clicked", "false");
+
+        node = node.data(nodes).enter().append("circle")
+          .attr("r", (d) => { return radius(d.textContent) || 5 })
+          .attr("fill", nodeColor)
+          .attr("clicked", "false")
+          .on("click", click)
+          .call(drag);
+
+        node.append("title")
+          .text(function (d) {
+            return d.label || d.id;
+          });
+
+        simulation.nodes(nodes);
+        simulation.force("link").links(links);
+        simulation.restart();
+    })
+    .catch((err) => {
+      throw err
+    })
+
+     // Functions that handle events
+     function nodeColor() {
+      return "#23affa";
+    }
+
+    function click() {
+      if (this.getAttribute("clicked") === "true") {
+        d3.select(this)
+        .transition()
+          .duration(500)
+          .attr("r", 5)
+          .attr("clicked", "false");
+
+          d3.selectAll("[clicked=false]").transition()
+          .duration(500)
+          .style("opacity", "1");
+          return
+      }
+
+      d3.select(this)
+      .each((d) => console.log(d.label, d.sex, d.dateOfBirth))
+      .attr("clicked", "true")
+      .transition()
+        .duration(500)
+        .attr("r", 30)
+        .style("opacity", "1");
+
+      d3.selectAll("[clicked=false]").transition()
+        .duration(500)
+        .style("opacity", ".3");
+    }
+
+    function ticked() {
+      link
+        .attr("x1", function (d) {
+          return d.source.x;
+        })
+        .attr("y1", function (d) {
+          return d.source.y;
+        })
+        .attr("x2", function (d) {
+          return d.target.x;
+        })
+        .attr("y2", function (d) {
+          return d.target.y;
+        });
+
+      node
+        .attr("cx", function (d) {
+          return d.x;
+        })
+        .attr("cy", function (d) {
+          return d.y;
+        });
+    }
+
+    function dragstarted(d) {
+      if (!d3.event.active) simulation.alphaTarget(.2).restart();
+      // d.fx = d.x, d.fy = d.y;
+    }
+
+    function dragged(d) {
+      // d.fx = d3.event.x, d.fy = d3.event.y;
+    }
+
+    function dragended(d) {
+      if (!d3.event.active) simulation.alphaTarget(0);
+      // d.fx = null, d.fy = null;
+    }
+  }
+
+
+  render() {
+    return (
+      <div className="Datasection" ref={this.myRef}>
+        <script src="https://d3js.org/d3.v4.min.js"></script>
+      </div>
+    );
+  }
 }
 
 export default Datasection;
