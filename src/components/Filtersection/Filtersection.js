b9eba19bebb8d01f63fcffe07ba03429ea5ca46e
Azam Awan <azam.awan@hva.nl>
2020-05-27 11:27:02 +0200

    :lipstick: Working on filter UI

--- src/components/Filtersection/Filtersection.js
+++ src/components/Filtersection/Filtersection.js
@@ -4,7 +4,47 @@ import './Filtersection.css';
 function Filtersection() {
   return (
     <div className="Filtersection">
-      <p>Filtersection</p>
+      <h1>"User"</h1>
+      <h3>"Afdeling"</h3>
+      <h3>"Function"</h3>
+          <br></br>
+      <h2>Filter</h2>
+          <br></br>
+
+
+
+          <table>
+<tbody>
+<tr>
+<td>Personen</td>
+<td>&nbsp;</td>
+</tr>
+<tr>
+<td>Vastgoed</td>
+<td>&nbsp;</td>
+</tr>
+<tr>
+<td>Niveau</td>
+<td>&nbsp;</td>
+</tr>
+<tr>
+<td>Relaties</td>
+<td>&nbsp;</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+</tr>
+<tr>
+<td>&nbsp;</td>
+<td>&nbsp;</td>
+</tr>
+</tbody>
+</table>
+
+
+
+      <h2>Weergave</h2>
     </div>
   );
 }
