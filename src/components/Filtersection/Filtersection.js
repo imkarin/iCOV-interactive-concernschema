281d54a0cabda0d248cbdcc0f5c7ff4968f6fd58
Azam Awan <azam.awan@hva.nl>
2020-05-27 16:11:13 +0200

    :lipstick: Improved UI

--- src/components/Filtersection/Filtersection.js
+++ src/components/Filtersection/Filtersection.js
@@ -4,47 +4,88 @@ import './Filtersection.css';
 function Filtersection() {
   return (
     <div className="Filtersection">
-      <h1>"User"</h1>
-      <h3>"Afdeling"</h3>
-      <h3>"Function"</h3>
-          <br></br>
-      <h2>Filter</h2>
-          <br></br>
+      
+      <section>
+          <h2>User</h2>
+
+          <h3>Afdeling</h3>
+          <h4>Functie</h4>
+            <br></br>
+      </section>
+
+      <section>
+          <h2>Filter</h2>
+            <br></br>
+
+            <table>
+              <tbody>
+
+                {/* Personen */}
+                <tr>
+                  <td>Personen</td>
+                  <td className="switchContainer">
+                      <label className="switch">
+                          <input type="checkbox"/>
+                          <span className="slider round"></span>
+                      </label>
+                  </td>
+                </tr>
+                {/* End Personen */}
+
+                {/* Vastgoed */}
+                <tr>
+                  <td>Vastgoed</td>
+                  <td className="switchContainer">
+                      <label className="switch">
+                          <input type="checkbox"/>
+                          <span className="slider round"></span>
+                      </label>
+                  </td>
+                </tr>
+                {/* End Vastgoed */}
+
+                {/* Niveau */}
+                <tr>
+                  <td>Niveau</td>
+                  <td className="switchContainer">
+                      <label className="switch">
+                          <input type="checkbox"/>
+                          <span className="slider round"></span>
+                      </label>
+                  </td>
+                </tr>
+                {/* End Niveau */}
+
+                {/* Relaties */}
+                <tr>
+                  <td>Relaties</td>
+                  <td className="switchContainer">
+                      <label className="switch">
+                          <input type="checkbox"/>
+                          <span className="slider round"></span>
+                      </label>
+                  </td>
+                </tr>
+                {/* End Relaties */}
+
+              </tbody>
+            </table>
+        </section>
+
+
+        <section>
+            <h2>Weergave</h2>
+
+        </section>
 
 
 
-          <table>
-<tbody>
-<tr>
-<td>Personen</td>
-<td>&nbsp;</td>
-</tr>
-<tr>
-<td>Vastgoed</td>
-<td>&nbsp;</td>
-</tr>
-<tr>
-<td>Niveau</td>
-<td>&nbsp;</td>
-</tr>
-<tr>
-<td>Relaties</td>
-<td>&nbsp;</td>
-</tr>
-<tr>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-</tr>
-<tr>
-<td>&nbsp;</td>
-<td>&nbsp;</td>
-</tr>
-</tbody>
-</table>
+      
+
+      
 
 
-
-      <h2>Weergave</h2>
+      
     </div>
   );
 }
