65920cf61e0b088790bac1092da7daac26a9bc78
karinmeijvogel <karinmeijvogel@hotmail.com>
2020-05-29 19:46:06 +0200

    :art: Moved data loading/updating functions to separate files

---
+++ src/assets/scripts/updateData.js
@@ -0,0 +1,23 @@
+import * as d3 from 'd3';
+
+export function updateData(thisComponent) {
+  const newFilters = thisComponent.props.filters
+
+  d3.selectAll("circle").filter((d) => { return !newFilters.includes(d.icovNodeType) })
+    .each(function(){
+      this.classList.add("filtered_out");
+    })
+
+    d3.selectAll("circle").filter((d) => { return newFilters.includes(d.icovNodeType) })
+      .each(function(){
+        this.classList.remove("filtered_out");
+      })
+
+  d3.selectAll("line").each(function(){
+    if (newFilters.length === 3) {
+      this.classList.remove("filtered_out");
+    } else {
+      this.classList.add("filtered_out");
+    }
+  })
+}
