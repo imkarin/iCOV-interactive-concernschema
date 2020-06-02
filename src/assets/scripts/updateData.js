fbe735c574bb21a824cac921dbb37b9f5228f0df
karinmeijvogel <karinmeijvogel@hotmail.com>
2020-06-02 13:13:46 +0200

    :bug: Fixed classlist bug in updateData function

--- src/assets/scripts/updateData.js
+++ src/assets/scripts/updateData.js
@@ -4,12 +4,12 @@ export function updateData(thisComponent) {
   const newFilters = thisComponent.props.filters
 
   d3.selectAll("circle").filter((d) => { return !newFilters.includes(d.icovNodeType) })
-    .each(() => this.classList.add("filtered_out"))
+    .each(function() { this.classList.add("filtered_out") })
 
   d3.selectAll("circle").filter((d) => { return newFilters.includes(d.icovNodeType) })
-    .each(() => this.classList.remove("filtered_out"))
+    .each(function() { this.classList.remove("filtered_out")})
 
-  d3.selectAll("line").each(() => {
+  d3.selectAll("line").each(function() {
     if (newFilters.length === 3) {
       this.classList.remove("filtered_out");
     } else {
