bd54a1422de7d29b6b2f9cf3450689a843b766d4
Azam Awan <azam.awan@hva.nl>
2020-06-04 14:50:58 +0200

    :lipstick: Updated UI and style files

---
+++ src/assets/scripts/nodeLabels.js
@@ -0,0 +1,21 @@
+
+
+import seniorMedewerkerMan from '../../assets/images/icons/Man.Sr.png';
+import juniorMedewerkerMan from '../../assets/images/icons/Man.Jr.png';
+
+import seniorMedewerkerVrouw from '../../assets/images/icons/Vrouw.Sr.png';
+import juniorMedewerkerVrouw from '../../assets/images/icons/Vrouw.Jr.png';
+
+import managerMan from '../../assets/images/icons/Man.Manager.png';
+import managerVrouw from '../../assets/images/icons/Vrouw.Manager.png';
+
+import nietWerkzaamMan from '../../assets/images/icons/Man.NW.png';
+import nietWerkzaamVrouw from '../../assets/images/icons/Vrouw.NW.png';
+
+
+import bedrijfActief from '../../assets/images/icons/Bedrijf.A.png';
+import bedrijfInactief from '../../assets/images/icons/Bedrijf.A.png';
+
+import adresWoonWerk from '../../assets/images/icons/WWAdres.png';
+
+import belastingsdienstDossier from '../../assets/images/icons/BDDossier.png';
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.css';
-import logo from '../../assets/images/iCOV.svg';
+import logo from '../../assets/images/Logo.png';
 
 function Header() {
   return (
